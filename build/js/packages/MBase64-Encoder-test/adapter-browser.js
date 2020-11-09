0be276a2e1a79e20ce3b0b27b54fd7b9c1f20da3
developer-- <jemo.mgebrishvili@gmail.com>
2020-11-09 23:25:30 +0400

    removed build cache files

--- build/js/packages/MBase64-Encoder-test/adapter-browser.js
+++
@@ -1,2 +0,0 @@
-require("/Users/mrkt/Documents/server/MBase64-Encoder/build/js/node_modules/kotlin-test-js-runner/kotlin-test-karma-runner.js")
-module.exports = require("/Users/mrkt/Documents/server/MBase64-Encoder/build/js/packages/MBase64-Encoder-test/kotlin/MBase64-Encoder-test.js")
